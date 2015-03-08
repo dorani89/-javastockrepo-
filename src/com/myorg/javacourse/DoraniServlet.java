1ae25fbdc802214bdfeffa02e96ed45ef0f8947f
dorani <dorani@dorani-PC>
2015-03-08 19:38:02 +0200

    second commit

--- src/com/myorg/javacourse/DoraniServlet.java
+++ src/com/myorg/javacourse/DoraniServlet.java
@@ -7,6 +7,6 @@ public class DoraniServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
-		
+		//change
 	}
 }
